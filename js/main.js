ab0e74c2d13834f80ee6923da98cc3b74befbe82
chhavi <s.chhavi@iitg.ac.in>
2018-04-23 11:55:18 +0530

    Add video filters+gallery!!

--- js/main.js
+++ js/main.js
@@ -17,6 +17,38 @@ function loader() {
 	});
 }
 
+/*------------------
+	Filter
+--------------------*/
+ 
+ $(function () {
+		
+	var filterList = {
+	
+		init: function () {
+		
+			// MixItUp plugin
+			// http://mixitup.io
+			$('#portfoliolist').mixItUp({
+				selectors: {
+  			  target: '.portfolio',
+  			  filter: '.filter'	
+  		  },
+  		  load: {
+    		  filter: '.app' // show app tab on first load
+    		}     
+			});								
+		
+		}
+
+	};
+	
+	// Run the show!
+	filterList.init();
+	
+});		
+
+
 
 
 /*------------------
